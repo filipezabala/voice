b62b611f7c5c63a70dfb3fdb5aa182bbfc58e117
Filipe Zabala <filipezabala@MacBook-Air-de-Filipe.local>
2020-01-26 00:31:59 -0300

    up

--- R/conv_df.R
+++ R/conv_df.R
@@ -9,11 +9,10 @@
 #' @param \code{mc.cores} The number of cores to mclapply.
 #' @return A vector of convoluted values with length near to \code{compact.to*length(x)}.
 #' @examples
-#' library(tidyverse)
 #' library(voice)
 #' x <- dat %>%
 #' mutate_each(as.factor, id:anyep_diff_w1)
-#' cx <- conv_df(x, 0.1)
+#' (cx <- conv_df(x, 0.1))
 #' dim(x)
 #' dim(cx)
 #' @seealso \code{conv}, \code{conv_mc}
@@ -29,13 +28,13 @@ conv_df <- function(x, compact.to, colnum = NULL, id = 'id', by.filter = id,
   }
 
   # non-numeric columns
-  # colnon <- setdiff(colnames(x), colnum)
+  colnon <- setdiff(colnames(x), colnum)
 
   # split numeric columns by.filter
   snum <- split(x[,colnum], x[,by.filter])
 
   # split non-numeric columns by.filter
-  # snon <- split(x[,colnon], x[,by.filter])
+  snon <- split(x[,colnon], x[,by.filter])
 
   # original lengths by.filter
   lv <- table(x[,by.filter])
@@ -49,25 +48,25 @@ conv_df <- function(x, compact.to, colnum = NULL, id = 'id', by.filter = id,
   cs <- c(0, cumsum(n))
 
   # convoluting numeric variables
-  ld <- lapply(snum, voice::conv_mc, compact.to = compact.to,
-               drop.zeros = drop.zeros, to.data.frame = T, mc.cores = mc.cores)
+  cn.li <- lapply(snum, voice::conv_mc, compact.to = compact.to,
+                  drop.zeros = drop.zeros, to.data.frame = T, mc.cores = mc.cores)
 
   # transforming in dataframe
-  ld.df <- lapply(ld, as.data.frame)
+  cn.df <- lapply(cn.li, as.data.frame)
 
   # compact list
   li <- vector('list', length = nid)
   for(i in 1:nid){
     index <- 1:n[i]
-    li[[i]] <- snon[[i]][index,]
-    li[[i]] <- dplyr::bind_cols(li[[i]], ld.df[[i]])
-    names(li)[i] <- nlv[i]
+    cn.li[[i]] <- snon[[i]][index,]
+    cn.li[[i]] <- dplyr::bind_cols(cn.li[[i]], cn.df[[i]])
+    names(cn.li)[i] <- nlv[i]
   }
 
   # compact dataframe
   if(to.data.frame){
-    li <- do.call(dplyr::bind_rows, li)
+    cn.li <- do.call(dplyr::bind_rows, cn.li)
   }
 
-  return(li)
+  return(cn.li)
 }
