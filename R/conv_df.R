5d905c68699ee545ed9ef49b13bd81229b7b36c1
Filipe Zabala <filipezabala@MacBook-Air-de-Filipe.local>
2020-01-26 00:38:33 -0300

    up

--- R/conv_df.R
+++ R/conv_df.R
@@ -2,11 +2,12 @@
 #'
 #' @param \code{x} A data frame.
 #' @param \code{compact.to} Percentage of remaining points after compactation. If equals to 1 and keep.zeros = T, the original vector is presented.
-#' @param \code{columns} A \code{char} vector indicating the colnames. If \code{NULL}, uses the columns of the \code{numeric} class.
-#' @param \code{by.filter} A \code{char} indicating the column to filter by. If \code{NULL} the filter is off.
+#' @param \code{colnum} A \code{char} vector indicating the numeric colnames. If \code{NULL}, uses the columns of the \code{numeric} class.
+#' @param \code{id} The identification column.
+#' @param \code{by.filter} A \code{char} indicating the column to filter by. If \code{NULL} uses the \code{id} content.
 #' @param \code{drop.zeros} Logical. Drop repeated zeros or compress to 1 zero per null set?
 #' @param \code{to.data.frame} Logical. Should the return be a data frame? If \code{F} returns a list.
-#' @param \code{mc.cores} The number of cores to mclapply.
+#' @param \code{mc.cores} The number of cores to mclapply. By default uses \code{parallel::detectCores()}.
 #' @return A vector of convoluted values with length near to \code{compact.to*length(x)}.
 #' @examples
 #' library(voice)
