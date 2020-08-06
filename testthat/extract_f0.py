d2d3eab3c8a4d79bd0419cf015029365876e604a
Filipe Zabala <filipezabala@MacBook-Air-de-Filipe.local>
2020-08-06 14:27:51 -0300

    sys.argv

--- testthat/extract_f0.py
+++ testthat/extract_f0.py
@@ -11,11 +11,11 @@ pandas.set_option('display.width', None)
 pandas.set_option('display.max_colwidth', None)
 dfs = []
 
-for file in os.listdir('/Library/Frameworks/R.framework/Versions/4.0/Resources/library/wrassp/extdata/'):
-# for file in os.listdir(sys.argv[1]):
+# for file in os.listdir('/Library/Frameworks/R.framework/Versions/4.0/Resources/library/wrassp/extdata/'):
+for file in os.listdir(sys.argv[1]):
   if file.endswith(".wav"):
-    file_list = os.path.join('/Library/Frameworks/R.framework/Versions/4.0/Resources/library/wrassp/extdata/', file)
-    # file_list = os.path.join(sys.argv[1], file)
+    # file_list = os.path.join('/Library/Frameworks/R.framework/Versions/4.0/Resources/library/wrassp/extdata/', file)
+    file_list = os.path.join(sys.argv[1], file)
     snd = parselmouth.Sound(file_list)
     pitch = snd.to_pitch(time_step = 5/1000)
     interval =  numpy.arange(pitch.start_time, pitch.end_time, 5/1000)
