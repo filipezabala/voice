fbd72cb5b7fc400ef4083883d1efebc9f5869d92
Filipe Zabala <filipezabala@MacBook-Air-de-Filipe.local>
2021-02-22 00:48:08 -0300

    sys.argv[0]?

--- tests/diarization-pyannote.py
+++ tests/diarization-pyannote.py
@@ -25,7 +25,7 @@ import warnings
 warnings.filterwarnings('ignore')
 
 
-def list_files(path = sys.argv[1], filter = sys.argv[2]):
+def list_files(path = sys.argv[0], filter = sys.argv[1]):
     """
     Lista todos os arquivos de um diretório e subdiretórios.
 
