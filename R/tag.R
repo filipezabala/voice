ed993285baaf36fe5a0398eb8ac38c4b9c0a917a
filipezabala <filipezabala@gmail.com>
2021-10-13 15:15:26 -0300

    Version 0.2.2

--- R/tag.R
+++ R/tag.R
@@ -2,11 +2,29 @@
 #'
 #' @param x A data frame to be tagged with media information.
 #' @param mediaDir Directory containing media files. Currently accepts audio files.
-#' @param id Column containing the ID to be searched in mediaDir file names. Default: \coe{NULL}, i.e., uses the first column.
-#' @param i ID position in \code{col}. Default: \code{4}.
+#' @param subj.id Column containing the subject ID to be searched in mediaDir file names. Default: \coe{NULL}, i.e., uses the first column.
+#' @param media.id Column containing the media ID. Default: \coe{NULL}, i.e., uses the first column.
+#' @param mc.cores Number of cores to be used
 #' @export
 tag <- function(x, mediaDir, subj.id = NULL, media.id = NULL,
-                mc.cores = 1){
+                mc.cores = 1, subj.id.simplify = FALSE,
+                filesRange = NULL, features = 'f0',
+                extraFeatures = FALSE,
+                gender = "u",
+                windowShift = 5,
+                numFormants = 8,
+                numcep = 12,
+                dcttype = c("t2", "t1", "t3", "t4"),
+                fbtype = c("mel", "htkmel", "fcmel", "bark"),
+                resolution = 40,
+                usecmp = FALSE,
+                full.names = TRUE,
+                recursive = FALSE,
+                check.mono = TRUE,
+                stereo2mono = TRUE,
+                overwrite = FALSE,
+                freq = 44100,
+                round.to = 4){
 
   if(is.null(subj.id)){
     subj.id <- 1
@@ -23,8 +41,30 @@ tag <- function(x, mediaDir, subj.id = NULL, media.id = NULL,
   x <- dplyr::as_tibble(x)
 
   # voice::feat_summary
-  fs <- voice::feat_summary(mediaDir, mc.cores = mc.cores)
+  fs <- voice::feat_summary(mediaDir, mc.cores = mc.cores,
+                            filesRange = filesRange,
+                            features = features,
+                            extraFeatures = extraFeatures,
+                            gender = gender,
+                            windowShift = windowShift,
+                            numFormants = numFormants,
+                            numcep = numcep,
+                            dcttype = dcttype,
+                            fbtype = fbtype,
+                            resolution = resolution,
+                            usecmp = usecmp,
+                            full.names = full.names,
+                            recursive = recursive,
+                            check.mono = check.mono,
+                            stereo2mono = stereo2mono,
+                            overwrite = overwrite,
+                            freq = freq,
+                            round.to = round.to)
   x <- dplyr::left_join(x, fs, by = 'file_name')
+
+  if(subj.id.simplify){
+    x[,subj.id] <- cumsum(!duplicated(x[,subj.id]))
+  }
 
   return(x)
 
