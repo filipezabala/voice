4a8d7ec1ea50a13e35d26d69cb6d430acafceb3c
Filipe Zabala <filipezabala@gmail.com>
2022-01-02 07:35:06 -0300

    Version 0.3.5.

---
+++ R/id_path.R
@@ -0,0 +1,15 @@
+#' Sample IDs and paths
+#'
+#' A dataset containing sample IDs and paths used in
+#' Zabala (2022) voice: new approaches to audio analysis.
+#' The considered sample contains 34,425 rows associated with
+#' 838 IDs ($p_{s} \approx 2.4\%$).
+#'
+#' @usage \code{data(id_path)}
+#' @format A tibble with 34,425 rows and 2 variables:
+#' \describe{
+#'   \item{client_id}{Hash containing client ID.}
+#'   \item{path}{Media file names.}
+#' }
+'id_path'
+
