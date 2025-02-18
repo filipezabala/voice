f5f91039f3191b2437b1c8787ebe6e7f25376d2f
fz <fz@fzs-MacBook-Air.local>
2025-02-18 11:00:39 -0300

    Version 0.5.0, dogv2 S4.

--- R/dog.R
+++ R/dog.R
@@ -26,47 +26,47 @@ setMethod("print", "Dog",
 Dog <- function(name, breed) {
   new("Dog", name = name, breed = breed)
 }
-
-# Example of another class inheriting from Dog
-setClass("HuntingDog",
-         slots = list(trainingLevel = "numeric"),
-         contains = "Dog") # Inherits from Dog
-
-# Add a method specific to HuntingDog
-setMethod("print", "HuntingDog",
-          function(x) {
-            callNextMethod() # Calls the print method of the parent class (Dog)
-            cat("Training Level:", x@trainingLevel, "\n")
-          })
-
-HuntingDog <- function(name, breed, trainingLevel) {
-  new("HuntingDog", name = name, breed = breed, trainingLevel = trainingLevel)
-}
-
-my_hunting_dog <- HuntingDog("Hunter", "German Shorthaired Pointer", 5)
-
-print(my_hunting_dog)
-
-# Example of using a generic function and dispatch
-
-# Define a generic function (can be used with different classes)
-setGeneric("dogBark", function(x) standardGeneric("dogBark"))
-
-# Define a method for the Dog class
-setMethod("dogBark", "Dog", function(x) {
-  print("Woof!")
-})
-
-# Define a method for the HuntingDog class (overriding the Dog method)
-setMethod("dogBark", "HuntingDog", function(x) {
-  print("WOOF WOOF! I'm a trained hunting dog!")
-})
-
-dogBark(my_dog)       # Output: Woof!
-dogBark(my_hunting_dog) # Output: WOOF WOOF! I'm a trained hunting dog!
-
-# Example of checking if an object belongs to a class
-is(my_dog, "Dog")          # TRUE
-is(my_hunting_dog, "Dog")  # TRUE (HuntingDog inherits from Dog)
-is(my_dog, "HuntingDog")   # FALSE
-is(my_hunting_dog, "HuntingDog") # TRUE
+#
+# # Example of another class inheriting from Dog
+# setClass("HuntingDog",
+#          slots = list(trainingLevel = "numeric"),
+#          contains = "Dog") # Inherits from Dog
+#
+# # Add a method specific to HuntingDog
+# setMethod("print", "HuntingDog",
+#           function(x) {
+#             callNextMethod() # Calls the print method of the parent class (Dog)
+#             cat("Training Level:", x@trainingLevel, "\n")
+#           })
+#
+# HuntingDog <- function(name, breed, trainingLevel) {
+#   new("HuntingDog", name = name, breed = breed, trainingLevel = trainingLevel)
+# }
+#
+# my_hunting_dog <- HuntingDog("Hunter", "German Shorthaired Pointer", 5)
+#
+# print(my_hunting_dog)
+#
+# # Example of using a generic function and dispatch
+#
+# # Define a generic function (can be used with different classes)
+# setGeneric("dogBark", function(x) standardGeneric("dogBark"))
+#
+# # Define a method for the Dog class
+# setMethod("dogBark", "Dog", function(x) {
+#   print("Woof!")
+# })
+#
+# # Define a method for the HuntingDog class (overriding the Dog method)
+# setMethod("dogBark", "HuntingDog", function(x) {
+#   print("WOOF WOOF! I'm a trained hunting dog!")
+# })
+#
+# dogBark(my_dog)       # Output: Woof!
+# dogBark(my_hunting_dog) # Output: WOOF WOOF! I'm a trained hunting dog!
+#
+# # Example of checking if an object belongs to a class
+# is(my_dog, "Dog")          # TRUE
+# is(my_hunting_dog, "Dog")  # TRUE (HuntingDog inherits from Dog)
+# is(my_dog, "HuntingDog")   # FALSE
+# is(my_hunting_dog, "HuntingDog") # TRUE
